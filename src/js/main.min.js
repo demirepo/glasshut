3567f047d334c2c93e7ee5fc5c6eefd8088db7de
Evgeny Petrunin <duen@mail.ru>
2022-06-02 10:23:36 +0300

    making burger

--- src/js/main.min.js
+++ src/js/main.min.js
@@ -3,20 +3,20 @@ import './tabs.js';
 import './lng-menu.js';
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  loop: true,
-  autoHeight: true,
-  loop: false,
+    // Optional parameters
+    loop: true,
+    autoHeight: true,
+    loop: false,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: 'true',
-  },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: 'true',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
